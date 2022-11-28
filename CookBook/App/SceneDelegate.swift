77d7577ee77497664065920e101827de5bc58d44
Shliakhin Sergei <sshlyahin@yahoo.com>
2022-11-28 20:19:33 +0300

    feat: Add OnboardingViewController

--- CookBook/App/SceneDelegate.swift
+++ CookBook/App/SceneDelegate.swift
@@ -9,16 +9,23 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    private let onboardingViewController = OnboardingViewController()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        onboardingViewController.delegate = self
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        viewController.view.backgroundColor = .orange
         
-        window?.rootViewController = viewController
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = onboardingViewController
         window?.makeKeyAndVisible()
     }
 }
 
+extension SceneDelegate: OnboardingViewControllerDelegate {
+    func didFinishOnboarding() {
+        print("Все ОК!")
+    }
+}
---
+++ CookBook/Controllers/OnboardingViewController.swift
@@ -0,0 +1,67 @@
+//
+//  OnboardingViewController.swift
+//  CookBook
+//
+//  Created by SERGEY SHLYAKHIN on 28.11.2022.
+//
+
+import UIKit
+
+protocol OnboardingViewControllerDelegate: AnyObject {
+    func didFinishOnboarding()
+}
+
+final class OnboardingViewController: UIViewController {
+
+    private let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.image = UIImage()
+        return imageView
+    }()
+    private let startButton = UIButton(type: .system)
+    
+    weak var delegate: OnboardingViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        applyStyle()
+        applyLayout()
+    }
+}
+
+extension OnboardingViewController {
+    private func setup() {
+        startButton.addTarget(self, action: #selector(startButtonTapped), for: .primaryActionTriggered)
+    }
+    
+    private func applyStyle() {
+        view.backgroundColor = .systemBackground
+        startButton.setTitle("Start", for: [])
+    }
+    
+    private func applyLayout() {
+        [backgroundImageView, startButton].forEach { item in
+            item.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(item)
+        }
+        
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+// MARK: - Actions
+extension OnboardingViewController {
+    @objc func startButtonTapped(_ sender: UIButton) {
+        delegate?.didFinishOnboarding()
+    }
+}
