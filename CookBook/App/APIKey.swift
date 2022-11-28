fb53eaef20467fcc888cae64903c1fb374ef9da6
Shliakhin Sergei <sshlyahin@yahoo.com>
2022-11-28 18:35:52 +0300

    style: Add new project structure

---
+++ CookBook/App/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  MovieQuiz
+//
+//  Created by SERGEY SHLYAKHIN on 18.11.2022.
+//
+
+import Foundation
+
+enum Secrets {
+    static let apiKey = "e9802a8e37424fb2830664ba7d44e04d"
+}
---
+++ CookBook/App/Theme.swift
@@ -0,0 +1,23 @@
+//
+//  Theme.swift
+//  MovieQuiz
+//
+//  Created by SERGEY SHLYAKHIN on 30.10.2022.
+//
+
+import UIKit
+
+enum Theme {
+    // MARK: - Fonts
+    
+    // MARK: - Colors
+    
+    // MARK: - Style
+    static let buttonCornerRadius: CGFloat = 15
+    static let imageCornerRadius: CGFloat = 20
+    
+    // MARK: - Layout
+    static let spacing: CGFloat = 20
+    static let leftOffset: CGFloat = 20
+    static let topOffset: CGFloat = 10
+}
